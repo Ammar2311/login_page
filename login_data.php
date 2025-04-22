f09007990282a79a7d8ba65ea211792729531f3e
Ammar Daoud <115896012+Ammar2311@users.noreply.github.com>
2025-04-22 18:13:55 +0300

    Add files via upload

---
+++ login_data.php
@@ -0,0 +1,23 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+<ul>
+    <li>User name:<?= $_POST['username']?></li>
+    <li>Email:<?= $_POST['email']?></li>
+    <li>Age:<?= $_POST['age']?></li>
+    <li>Password:<?= $_POST['password']?></li>
+
+</ul>
+    
+</body>
+</html>
